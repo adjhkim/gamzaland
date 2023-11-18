18979e7dd77d8b13c52a51a1fc6c408ff20a3806
adjhkim <adjhkim568@gmail.com>
2023-11-18 17:37:40 +0900

    Main Page 레이아웃 설정

--- gamzaland/src/app/pages/HomePage/index.tsx
+++ gamzaland/src/app/pages/HomePage/index.tsx
@@ -1,14 +1,73 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #eee;
+`;
+
+const Box = styled.div`
+  width: 360px;
+  height: 640px;
+  background-color: #fff;
+  box-shadow: 0px 0px 100px -60px rgba(0, 0, 0, 0.5);
+
+  @media (max-width: 600px) {
+    width: 100%;
+    height: 100vh;
+  }
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 10%;
+  background-color: #f7be81;
+`;
+
+const ImportantNotice = styled.div`
+  width: 90%;
+  height: 40%;
+  margin: 4.5% 5%;
+  background-color: #eee;
+  border-radius: 8px;
+`;
+
+const ImportantSchedule = styled.div`
+  width: 90%;
+  height: 25%;
+  margin: 4.5% 5%;
+  background-color: #eee;
+  border-radius: 8px;
+`;
+
+const IconList = styled.div`
+  width: 90%;
+  height: 15%;
+  margin: 4.5% 5%;
+  background-color: #eee;
+  border-radius: 8px;
+`;
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>GamzaLand</title>
+        <meta name="description" content="gamzaland" />
       </Helmet>
-      <span>My HomePage</span>
+      <Background>
+        <Box>
+          <Header></Header>
+          <ImportantNotice></ImportantNotice>
+          <ImportantSchedule></ImportantSchedule>
+          <IconList></IconList>
+        </Box>
+      </Background>
     </>
   );
 }
